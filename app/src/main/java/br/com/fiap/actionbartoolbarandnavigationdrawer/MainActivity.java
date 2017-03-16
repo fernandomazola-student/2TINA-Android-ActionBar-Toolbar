621be83a02825f446863baf51efac56c9b384d6e
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-16 10:27:12 -0300

    Adicionando criado arquivo menu e adicionado itens

--- app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
+++ app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.fiap.actionbartoolbarandnavigationdrawer;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Pegando a instancia, pegando o elemento da variavel para manipular ele
+        ActionBar actionBar = getSupportActionBar();
+        //Mundando o elemento titulo
+        actionBar.setTitle("Exemplo");
+
+    }
+
+    //Metodo da actiivty
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Vamos sobrescrever para criar o menuzinho la em cima
+        //Vamos falar qual xml de menu que queremos, vamos inflar o xml
+        //Inflar = ler um xml e transformar em objeto de java
+        return super.onCreateOptionsMenu(menu);
     }
 }
