b598e086eb811eda78c2aaef4add2bcca999215e
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-16 11:12:49 -0300

    Adicionado toolbar principal dentro da activity

--- app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/TelaCheiaActivity.java
+++ app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/TelaCheiaActivity.java
@@ -2,6 +2,7 @@ package br.com.fiap.actionbartoolbarandnavigationdrawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TelaCheiaActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class TelaCheiaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cheia);
+        //Atrelando o componente xml com uma variavel
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        //Falando qual toolbar é a principal dentro da activity
+        setSupportActionBar(toolbar);
     }
 }
