84b0116e7a81882c23cbe8a0cc5d83846a749812
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-16 10:34:21 -0300

    Adicionado item no actionbar e adicionando metodo que pega elemento quando clicado

--- app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
+++ app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         //Vamos sobrescrever para criar o menuzinho la em cima
         //Vamos falar qual xml de menu que queremos, vamos inflar o xml
         //Inflar = ler um xml e transformar em objeto de java
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //Pegando os cliques nos elementos que estao na actionbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.mnSobre){
+            Toast.makeText(this, "Clicou em sobre", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
